47423c297c2d36048a419ef361abb732033019f7
pathbox <1064569731@qq.com>
2017-06-25 12:40:24 +0800

    动态规划之入门-爬阶梯问题

---
+++ dynamic_programming/爬楼梯问题/simple_recursion.go
@@ -0,0 +1,29 @@
+// 简单递归法
+
+// F(1) = 1;
+// F(2) = 2;
+// F(n) = F(n-1)+F(n-2)（n>=3）
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	n := 10
+	result := getClimbingWays(n)
+	fmt.Println("The result is: ", result)
+}
+
+func getClimbingWays(n int) int {
+	if n < 1 {
+		return 0
+	} else if n == 1 {
+		return 1
+	} else if n == 2 {
+		return 2
+	}
+
+	return getClimbingWays(n-1) + getClimbingWays(n-2)
+}
