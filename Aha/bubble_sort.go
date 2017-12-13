5aaf6430a6acf23852849b24aee1ac0df6aa28f9
pathbox <1064569731@qq.com>
2017-12-13 20:31:17 +0800

    冒泡排序优化方法 减少了外层循环的次数。因为其实已经有序了，之后的外层循环可以不用再继续进行

--- Aha/bubble_sort.go
+++ Aha/bubble_sort.go
@@ -11,11 +11,12 @@ func main() {
 
 	array_len := len(array)
 
-	for i := 1; i < array_len; i++ { // 9次 len-1
-		for j := 0; j < array_len-i; j++ { // j j+1的值范围是[0-9]
+	for i := 1; i < array_len; i++ { // 9次 len-1  i 从1开始，只是为了 👇的array_len - i
+		for j := 0; j < array_len-i; j++ { // j j+1的值范围是[0-9] j从0开始进行，循环 len-1次
 			if array[j] > array[j+1] {
 				array[j], array[j+1] = array[j+1], array[j]
 			}
+			fmt.Println(j, "-", array)
 		}
 	}
 
