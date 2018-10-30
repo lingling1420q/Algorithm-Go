544fc04f68e54263351db08ec22a1d3c74c7438a
pathbox <cary.luo@ucloud.cn>
2018-10-30 14:37:10 +0800

    understand merge sort

---
+++ sort/merge_sort/merge_sort_1.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func mergeSort(r []int) []int {
+	length := len(r)
+	if length <= 1 { // 先递归按中间不断拆分，到数组r长度为1，然后递归出栈进行排序合并
+		return r
+	}
+	mid := length / 2
+	left := mergeSort(r[:mid])
+	right := mergeSort(r[mid:])
+	return merge(left, right)
+}
+
+func merge(left, right []int) (result []int) {
+	l, r := 0, 0
+	for l < len(left) && r < len(right) { // 停止的条件其实是，left或right数组有一个遍历完
+		if left[l] < right[r] { // left数组的值更小，取left的值
+			result = append(result, left[l])
+			l++
+		} else { // right数组的值更小，取right的值
+			result = append(result, right[r])
+			r++
+		}
+	}
+	// 剩下的数肯定都大于(或小于)result中的数
+	result = append(result, left[l:]...)  // 如果 left还有没遍历的，全部加到result尾部
+	result = append(result, right[r:]...) // 如果 right还有没遍历的，全部加到result尾部， 这两步只会有一步有append到result
+	return
+}
+
+func main() {
+	a := []int{9, 4, 2, 5, 6, 7, 1, 0, 3, 11, 75, 23}
+	fmt.Println("Before Sort: ", a)
+	result := mergeSort(a)
+	fmt.Println("After Sort:  ", result)
+}
