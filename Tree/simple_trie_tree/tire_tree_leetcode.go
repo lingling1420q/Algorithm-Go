015d9ce3d9c4496e917fc1c8390cb7c5eddfe4c1
pathbox <cary.luo@ucloud.cn>
2020-01-19 18:42:59 +0800

    tire_tree_leetcode

---
+++ Tree/simple_trie_tree/tire_tree_leetcode.go
@@ -0,0 +1,77 @@
+package leetcodetrie
+
+type Trie struct {
+	Root *TrieNode
+}
+
+type TrieNode struct {
+	Children map[rune]*TrieNode
+	End      bool
+}
+
+func NewTrie() *Trie {
+	t := &Trie{}
+	t.Root = NewTrieNode()
+	return t
+}
+
+func NewTrieNode() *TrieNode {
+	n := &TrieNode{}
+	n.Children = make(map[rune]*TrieNode)
+	n.End = false
+	return n
+}
+
+/** Initialize your data structure here. */
+func Constructor() Trie {
+	t := Trie{}
+	t.Root = NewTrieNode()
+	return t
+}
+
+func (t *Trie) Insert(word string) {
+	if len(word) < 1 {
+		return
+	}
+	chars := []rune(word)
+	slen := len(chars)
+	node := t.Root
+	for i := 0; i < slen; i++ { // 将word的每个词分割,作为key, 遍历word
+		if _, exist := node.Children[chars[i]]; !exist {
+			node.Children[chars[i]] = NewTrieNode() // 如果不存在则用这个key新建结点
+		}
+		node = node.Children[chars[i]] // 将node指针 移到子节点上
+	}
+	node.End = true
+}
+
+/** Returns if the word is in the trie. */
+func (this *Trie) Search(word string) bool {
+	chars := []rune(word)
+	slen := len(chars)
+	node := this.Root
+	for i := 0; i < slen; i++ {
+		if _, exists := node.Children[chars[i]]; !exists {
+			return false
+		}
+		node = node.Children[chars[i]] // 将node指针移到子节点上
+		if node.End == true && i == slen-1 {
+			return true
+		}
+	}
+	return false
+}
+
+/** Returns if there is any word in the trie that starts with the given prefix. */
+func (this *Trie) StartsWith(prefix string) bool {
+	chars := []rune(prefix)
+	slen := len(chars)
+	node := this.Root
+	for i := 0; i < slen; i++ {
+		if _, exists := node.Children[chars[i]]; !exists {
+			return false // 只要有一个不匹配 则说明不是prefix
+		}
+		node = node.Children[chars[i]]
+	}
+	return true
+}
